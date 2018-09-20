77fe6f8d811ae0b9117bcb3e81765b8e75e832bb
d769246859@163.com <d769246859@163.com>
2018-09-20 16:54:52 +0800

    新增卡夫卡相关

---
+++ linxu-redis/src/main/java/com/example/linxuredis/LinxuRedisApplication.java
@@ -0,0 +1,12 @@
+package com.example.linxuredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinxuRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinxuRedisApplication.class, args);
+    }
+}
